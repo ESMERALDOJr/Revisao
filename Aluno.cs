a5139b3f34c3d78ad2e07db98e60f3213a7d279e
ESMERALDOJr <esmeraldo.log@hotmail.com>
2021-12-29 09:10:24 -0300

    Praticando primeiros passos em C#

---
+++ Aluno.cs
@@ -0,0 +1,8 @@
+namespace Revisao
+{
+    public struct Aluno
+    {
+        public string Nome { get; set; } // foi usado Prop para cria uma propriedade
+        public decimal Nota { get; set; } // foi usado Prop para cria uma propriedade
+    }
+}
